5f0806bdab10708cd55de807ac282a09a1c1879a
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-03-14 17:27:31 +0300

    Initial Commit

---
+++ HomeWorkLesson3.6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWorkLesson3.6
+//
+//  Created by Олейник Богдан on 14.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWorkLesson3.6/HomeWorkLesson3_6App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkLesson3_6App.swift
+//  HomeWorkLesson3.6
+//
+//  Created by Олейник Богдан on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkLesson3_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
